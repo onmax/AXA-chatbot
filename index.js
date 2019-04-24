10e06f14f7a337ffebbb1beb34ddf094480960ec
onmax <maximogarciamtnez@gmail.com>
2019-04-24 13:00:30 +0200

    manager

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const { Bot } = require('./bot');
 const { BotAdapterTgm } = require('./bot-adapter-tgm');
-<<<<<<< HEAD
 const { NlpManager } = require('node-nlp');
 const trainnlp = require('./train/train-nlp');
 
@@ -12,8 +11,3 @@ const manager = new NlpManager({languages: ['es']});
     const bot = new Bot(manager);
     new BotAdapterTgm(bot.processMessage).start();
 })();
-=======
-console.log("Starting up bot!");
-const bot = new Bot();
-new BotAdapterTgm(bot.processMessage).start();
->>>>>>> 9bbea8632a8f3ff79c99d510d5c1f86e3c575d91
