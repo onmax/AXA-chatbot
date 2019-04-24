ab9fcbe10bc60fc15a54d78ff1b9d5e3b9a07f05
Daniel <mdanielm@protonmail.com>
2019-04-24 14:42:21 +0200

    Write data to redis

--- redis.js
+++ redis.js
@@ -1,5 +1,4 @@
-var redis = require("redis"),
-    client = redis.createClient();
+
 
 // if you'd like to select database 3, instead of 0 (default), call
 // client.select(3, function() { /* ... */ });
@@ -8,17 +7,34 @@ client.on("error", function (err) {
     console.log("Error " + err);
 });
 
+module.exports.KEY_DNI = 'dni';
+module.exports.KEY_NAME = 'name';
+module.exports.KEY_AFFECTED = 'affected';
+module.exports.KEY_INFO = 'info';
+module.exports.KEY_PLACE = 'place';
+module.exports.KEY_PHOTO_INCIDENT = 'incident';
+module.exports.KEY_PHOTO_PARTE = 'parte';
+module.exports.KEY_MATRICULA = 'matricula';
+module.exports.KEY_MODEL = 'car_model';
+module.exports.KEY_DRIVER_LICENSE_YEAR = 'driver_license_year';
+module.exports.KEY_VEHICLE_DATA = 'key_vehicle_data';
 
-function setValue(id, chatid) {
+
+
+function setValue(chatid, key, value) {
+    var redis = require("redis"),
+        client = redis.createClient();
+    return new Promise(function (fulfill, reject){
+            client.hset(`user.${chatid}`, key, value, function (err, reply) {
+                console.log(reply.length + " replies:");
+                if (err) reject(err);
+                else fulfill(reply);
+                client.quit();
+            });
+
+    });
+
 
 }
-client.set("string key", "string val", redis.print);
-client.hset("hash key", "hashtest 1", "some value", redis.print);
-client.hset(["hash key", "hashtest 2", "some other value"], redis.print);
-client.hkeys("hash key", function (err, replies) {
-    console.log(replies.length + " replies:");
-    replies.forEach(function (reply, i) {
-        console.log("    " + i + ": " + reply);
-    });
-    client.quit();
-});
+
+setValue('asd', KEY_DNI, '5367282Y',)
